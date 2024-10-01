efc9bb2b42df38a792ec12f2154178e2bea0fca9
paulo.guerra <paulo.guerra@flex-am.com.br>
2024-10-01 13:47:32 -0400

    fix: alternate api base url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 
 import { Nunito_Sans as NunitoSans } from 'next/font/google'
+import Head from 'next/head'
 
 import { SessionWrapper } from './components/session-wrapper'
 
@@ -16,6 +17,9 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <Head>
+        <link rel="icon" href="/bookwise-icon.svg" />
+      </Head>
       <body className={`${nunitoSans.className} bg-gray-800 antialiased`}>
         <SessionWrapper>{children}</SessionWrapper>
       </body>
--- src/app/lib/axios.ts
+++ src/app/lib/axios.ts
@@ -1,5 +1,8 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL:
+    process.env.NODE_ENV === 'production'
+      ? 'https://book-wise-villo.vercel.app/api'
+      : 'http://localhost:3000/api',
 })
